c987c4cca80ea033b88caf9d35ccd6da5a34d453
ZhanG_b1nG <zhangbingbing5631@163.com>
2021-08-07 19:40:54 +0800

    #202108071940 测试免密推送

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * helloWorld2.0
+ * helloWorld2.1
  *
  * @Author ZhanG_b1nG
  * @Date 2020/4/7 22:16
